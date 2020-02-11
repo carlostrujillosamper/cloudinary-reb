4105cb4d0c7e9620abf8dc81ee01d95febbf97c2
carlos.trujillo <carlos@riverettecom>
2020-02-11 14:22:05 +0100

    abstracted mapping on csv route

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express      = require('express');
-const path         = require('path');
+const express = require("express");
+const path = require("path");
 
-
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
-
-
-const cloudRoutes = require('./routes/cloudinary.routes')
-app.use('/cloudinary', cloudRoutes)
-
+const cloudRoutes = require("./routes/cloudinary.routes");
+app.use("/cloudinary", cloudRoutes);
 
 module.exports = app;
--- routes/cloudinary.routes.js
+++ routes/cloudinary.routes.js
@@ -1,12 +1,11 @@
-const express = require('express');
-const router  = express.Router();
-const cloudinary = require('../configs/cloudinary.config');
-const fs = require('fs');
-const path = require('path');
-const {convertArrayToCSV} = require('convert-array-to-csv');
+const express = require("express");
+const router = express.Router();
+const cloudinary = require("../configs/cloudinary.config");
+const fs = require("fs");
+const path = require("path");
+const { convertArrayToCSV } = require("convert-array-to-csv");
 
-const filename = path.join(__dirname, 'allImages.csv')
-
+const filename = path.join(__dirname, "allImages.csv");
 
 router.get("/statistics", (req, res) => {
   let statistics = {
@@ -15,44 +14,48 @@ router.get("/statistics", (req, res) => {
     biggestPicture: "",
     smallestPicture: "",
     avgSize: ""
-  }
-  let allImages = []
+  };
+  let allImages = [];
   const getStatistics = nextPage => {
     cloudinary.v2.api
       .resources({ max_results: 500, next_cursor: nextPage })
       .then(images => {
-        allImages.push(...images.resources)
+        allImages.push(...images.resources);
         if (!images.next_cursor) {
-          statistics.totalImages = allImages.length
-          allImages.forEach(image => statistics.formats[image.format]==undefined ? statistics.formats[image.format]=1 :++statistics.formats[image.format])
+          statistics.totalImages = allImages.length;
+          allImages.forEach(image =>
+            statistics.formats[image.format] == undefined
+              ? (statistics.formats[image.format] = 1)
+              : ++statistics.formats[image.format]
+          );
           let maxSize = allImages.reduce(
             (prev, current) => (prev.bytes > current.bytes ? prev : current),
             1
-          )
-          statistics.biggestPicture = maxSize.url
+          );
+          statistics.biggestPicture = maxSize.url;
           let minSize = allImages.reduce(
             (prev, current) => (prev.bytes < current.bytes ? prev : current),
             1
-          )
-          statistics.smallestPicture = minSize.url
+          );
+          statistics.smallestPicture = minSize.url;
           statistics.avgSize = Math.floor(
             allImages.reduce((total, current) => total + current.bytes, 0) /
               allImages.length
-          )
-          res.json(statistics)
-          console.log(statistics)
+          );
+          res.json(statistics);
+          console.log(statistics);
         } else {
-          getStatistics(images.next_cursor)
+          getStatistics(images.next_cursor);
         }
       })
-      .catch(err => console.log(err))
-  }
+      .catch(err => console.log(err));
+  };
 
-  getStatistics()
-})
+  getStatistics();
+});
 
 router.get("/csv", (req, res) => {
-  let allImagesDetailed = []
+  let allImagesDetailed = [];
   const getCsv = nextPage => {
     cloudinary.v2.search
       .expression("")
@@ -60,32 +63,34 @@ router.get("/csv", (req, res) => {
       .next_cursor(nextPage)
       .execute()
       .then(imagesDetailed => {
-        allImagesDetailed.push(...imagesDetailed.resources)
+        allImagesDetailed.push(...imagesDetailed.resources);
         if (!imagesDetailed.next_cursor) {
-          let allImagesDetailedFixed = allImagesDetailed.map(imageDetailed => ({
-            ...imageDetailed,
-            created_by: JSON.stringify(imageDetailed.created_by).replace(
-              /[{}"]/g,
-              ""
-            ),
-            uploaded_by: JSON.stringify(imageDetailed.uploaded_by).replace(
-              /[{}"]/g,
-              ""
-            )
-          }))
-          let csvFromArrayOfArrays = convertArrayToCSV(allImagesDetailedFixed)
-          fs.writeFileSync(filename, csvFromArrayOfArrays, "utf8")
-          res.attachment("allImages.csv")
-          res.send(Buffer.from(csvFromArrayOfArrays))
-          console.log('CSV file created')
+          let allImagesDetailedFixed = allImagesDetailed.map(imagesDetailed => {
+            Object.keys(imagesDetailed).forEach(key => {
+              if (
+                typeof imagesDetailed[key] == "object" &&
+                imagesDetailed[key] != null
+              ) {
+                imagesDetailed[key] = JSON.stringify(
+                  imagesDetailed[key]
+                ).replace(/[{}"]/g, "");
+              }
+            });
+            return { ...imagesDetailed };
+          });
+          let csvFromArrayOfArrays = convertArrayToCSV(allImagesDetailedFixed);
+          fs.writeFileSync(filename, csvFromArrayOfArrays, "utf8");
+          res.attachment("allImages.csv");
+          res.send(Buffer.from(csvFromArrayOfArrays));
+          console.log("CSV file created");
         } else {
-          getCsv(imagesDetailed.next_cursor)
+          getCsv(imagesDetailed.next_cursor);
         }
       })
-      .catch(err => console.log(err))
-  }
-  getCsv()
-})
+      .catch(err => console.log(err));
+  };
+  getCsv();
+});
 
 
 
